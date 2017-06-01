415cbd0c4776a4d7f48143f4d73111831413deb0
chenjunhui <chenjunhui@miguantech.com>
2017-06-01 16:56:50 +0800

    品牌主页更多品牌 修复n多bug

--- app/src/main/java/com/miguan/yjy/module/product/StarProductFragment.java
+++ app/src/main/java/com/miguan/yjy/module/product/StarProductFragment.java
@@ -1,14 +1,22 @@
 package com.miguan.yjy.module.product;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.dsk.chain.bijection.RequiresPresenter;
 import com.dsk.chain.expansion.list.BaseListFragment;
 import com.dsk.chain.expansion.list.ListConfig;
+import com.jude.easyrecyclerview.EasyRecyclerView;
 import com.jude.easyrecyclerview.adapter.BaseViewHolder;
 import com.miguan.yjy.R;
 import com.miguan.yjy.adapter.viewholder.SearchReslutViewHolder;
 import com.miguan.yjy.model.bean.Product;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * @作者 cjh
@@ -19,10 +27,14 @@ import com.miguan.yjy.model.bean.Product;
 public class StarProductFragment extends BaseListFragment<StarProductPresenter, Product> {
 
 
+    @BindView(R.id.recycle)
+    EasyRecyclerView mRecycle;
+    Unbinder unbinder;
+
 
     @Override
     public BaseViewHolder<Product> createViewHolder(ViewGroup parent, int viewType) {
-        return new SearchReslutViewHolder(parent,1);
+        return new SearchReslutViewHolder(parent, 1);
     }
 
     @Override
@@ -32,6 +44,21 @@ public class StarProductFragment extends BaseListFragment<StarProductPresenter, 
 
     @Override
     public ListConfig getListConfig() {
-        return super.getListConfig().setLoadMoreAble(false);
+
+        return super.getListConfig().setLoadMoreAble(false).setNoMoreAble(false);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/main/java/com/miguan/yjy/module/product/StarProductPresenter.java
+++ app/src/main/java/com/miguan/yjy/module/product/StarProductPresenter.java
@@ -1,11 +1,26 @@
 package com.miguan.yjy.module.product;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.dsk.chain.expansion.list.BaseListFragmentPresenter;
+import com.jude.easyrecyclerview.adapter.RecyclerArrayAdapter;
+import com.miguan.yjy.R;
+import com.miguan.yjy.adapter.BrandMoreAdapter;
 import com.miguan.yjy.adapter.BrandPagerAdapter;
 import com.miguan.yjy.model.ProductModel;
+import com.miguan.yjy.model.bean.Brand;
+import com.miguan.yjy.model.bean.BrandAll;
 import com.miguan.yjy.model.bean.Product;
+import com.miguan.yjy.model.services.ServicesResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
 
 /**
  * @作者 cjh
@@ -15,6 +30,8 @@ import com.miguan.yjy.model.bean.Product;
 
 public class StarProductPresenter extends BaseListFragmentPresenter<StarProductFragment, Product> {
     long brandId;
+
+    private List<Brand> brandList = new ArrayList<>();
 
     @Override
     protected void onCreate(StarProductFragment view, Bundle saveState) {
@@ -26,6 +43,30 @@ public class StarProductPresenter extends BaseListFragmentPresenter<StarProductF
     @Override
     protected void onCreateView(StarProductFragment view) {
         super.onCreateView(view);
+        ProductModel.getInstance().getBrandInfo(brandId).subscribe(new ServicesResponse<BrandAll>() {
+            @Override
+            public void onNext(BrandAll brandAll) {
+                super.onNext(brandAll);
+                getAdapter().addFooter(new RecyclerArrayAdapter.ItemView() {
+                    @Override
+                    public View onCreateView(ViewGroup parent) {
+                        ButterKnife.bind(this, parent);
+                        View moreBrand = View.inflate(getView().getActivity(), R.layout.foot_more_brand, null);
+                        RecyclerView  recy_brand_more = (RecyclerView) moreBrand.findViewById(R.id.recy_brand_more);
+                        recy_brand_more.setLayoutManager(new LinearLayoutManager(getView().getActivity(), LinearLayoutManager.HORIZONTAL, false));
+                        BrandMoreAdapter brandMoreAdapter = new BrandMoreAdapter(getView().getActivity(), brandAll.getOtherBrand());
+                        recy_brand_more.setAdapter(brandMoreAdapter);
+                        brandMoreAdapter.notifyDataSetChanged();
+                        return moreBrand;
+                    }
+
+                    @Override
+                    public void onBindView(View headerView) {
+
+                    }
+                });
+            }
+        });
     }
 
     @Override
