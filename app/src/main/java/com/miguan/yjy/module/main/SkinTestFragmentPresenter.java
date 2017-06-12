314c30d5b2460571fcae49ac61a641fa53e8b0fd
chenjunhui <chenjunhui@miguantech.com>
2017-06-12 11:18:26 +0800

    肤质测试失败问题

--- app/src/main/java/com/miguan/yjy/module/main/SkinTestFragmentPresenter.java
+++ app/src/main/java/com/miguan/yjy/module/main/SkinTestFragmentPresenter.java
@@ -1,9 +1,7 @@
 package com.miguan.yjy.module.main;
 
 import com.dsk.chain.expansion.data.BaseDataFragmentPresenter;
-import com.miguan.yjy.model.TestModel;
 import com.miguan.yjy.model.bean.Test;
-import com.miguan.yjy.model.local.UserPreferences;
 
 /**
  * @作者 cjh
@@ -13,18 +11,5 @@ import com.miguan.yjy.model.local.UserPreferences;
 
 public class SkinTestFragmentPresenter extends BaseDataFragmentPresenter<SkinTestFragment, Test> {
 
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        loadData();
-    }
-
-
-    public void loadData() {
-        if (UserPreferences.getUserID() > 0) {
-            TestModel.getInstantce().getSkinRecommend().unsafeSubscribe(getSubscriber());
-        }
-    }
 
 }
